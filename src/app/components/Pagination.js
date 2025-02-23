535cb71d715fef6ffae869357413ffdaef47dfb6
rostikcheremys <rostikcheremys@gmail.com>
2025-02-23 15:45:36 +0200

    'use client'

--- src/app/components/Pagination.js
+++ src/app/components/Pagination.js
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 export default function Pagination({totalPages, currentPage, setCurrentPage}) {
     const generatePageNumbers = () => {
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
-"use client";
+'use client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import "./components/styles.css";
+import './components/styles.css'
 
-import NavBar from "@/app/components/NavBar";
-import Footer from "@/app/components/Footer";
+import NavBar from '@/app/components/NavBar';
+import Footer from '@/app/components/Footer';
 
 export default function DashboardLayout({ children }) {
     return (
