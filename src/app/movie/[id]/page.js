51dad35ffd38294ce59230fc2849cc379063e0b7
rostikcheremys <rostikcheremys@gmail.com>
2025-03-01 15:00:51 +0200

    add line-item

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -59,18 +59,28 @@ export default function Page() {
                             <a href={movie.homepage}>{movie.title}</a>
                         </div>
 
+                        <div className="line-item"></div>
+
                         <div className="card-info-item">
-                            <p><i className="bi bi-tags-fill"></i> {movie.genres.map((genre) => genre.name).join(", ")}</p>
-                            <p><i className="bi bi-calendar2-week-fill"></i> {movie.release_date}</p>
-                            <p><i className="bi bi-clock-fill"></i> {movie.runtime} minutes</p>
-                            <p><i className="bi bi-globe2"></i> {movie.spoken_languages.map((lang) => lang.english_name).join(", ")}</p>
                             <p><i className="bi bi-star-fill"></i> {movie.popularity}</p>
-                            <p><i className="bi bi-geo-alt-fill"></i> {movie.production_countries.map((country) => country.name).join(", ")}</p>
-                            <p><i className="bi bi-building-fill"></i> {movie.production_companies.map((company) => company.name).join(", ")}</p>
+                            <p><i className="bi bi-tags-fill"></i> {movie.genres.map((genre) => genre.name).join(", ")}
+                            </p>
+                            <p><i className="bi bi-clock-fill"></i> {movie.runtime} minutes</p>
+                            <p><i
+                                className="bi bi-globe2"></i> {movie.spoken_languages.map((lang) => lang.english_name).join(", ")}
+                            </p>
+                            <p><i
+                                className="bi bi-geo-alt-fill"></i> {movie.production_countries.map((country) => country.name).join(", ")}
+                            </p>
+                            <p><i
+                                className="bi bi-building-fill"></i> {movie.production_companies.map((company) => company.name).join(", ")}
+                            </p>
+                            <p><i className="bi bi-calendar2-week-fill"></i> {movie.release_date}</p>
                         </div>
                     </div>
                 </div>
             </div>
+
 
             {trailer ? (
                 <div className="trailer-container">
